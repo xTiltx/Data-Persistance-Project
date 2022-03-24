d92507b77a74313d8edaf1fcc32b799ec3a80ed7
xTiltx <89490871+xTiltx@users.noreply.github.com>
2022-03-24 15:14:57 -0700

    Added Main Menu + Transitions

---
+++ Assets/Scripts/UIMainScene.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class UIMainScene : MonoBehaviour
+{
+
+    public TMP_Text highScoreText;
+    public InputField nameInputField;
+    public Button startButton;
+    public Button exitButton;
+
+    private void Start()
+    {
+        
+    }
+
+    public void StartNew()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Exit()
+    {
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#else
+        Application.Quit();
+#endif
+    }
+}
